c23bb8e9cf79653cf3983c55a4aed9737dac6ee8
ykhade <ykhade@nevada.unr.edu>
2023-11-26 23:12:32 -0800

    Add detailed comments to baseScraper.js methods
    Expanded the baseScraper.js functionality by adding comprehensive comments for future developers. The comments include information about the purpose of methods, the expected outcomes, and any potential errors. The aim is to improve the readability and maintainability of the code by providing clearer explanations about what each function does.

--- core/baseScraper.js
+++ core/baseScraper.js
@@ -1,24 +1,49 @@
 const playwright = require('playwright');
 
+/**
+ * Class representing a base scraper.
+ * @class
+ */
 class BaseScraper {
   constructor(url) {
     this.url = url;
   }
 
-  async launchBrowser() {
+  /**
+   * Launches a browser and initializes a new page.
+   * @async
+   * @function
+   * @name launchBrowser
+   * @returns {void}
+   */
+  launchBrowser = async() => {
     this.browser = await playwright.firefox.launch({headless: false});
     this.page = await this.browser.newPage();
     console.log("Browser Initialized");
   }
 
-  async goToPage() {
+
+  /**
+   * Navigate to a specific page.
+   * @memberof BaseScraper
+   * @throws {Error} If browser is not initialized. Call launchBrowser first.
+   * @returns {Promise<void>} Resolves after navigating to the page.
+   */
+  goToPage = async() => {
     if (!this.page) {
       throw new Error('Browser is not initialized. Call launchBrowser first.');
     }
     await this.page.goto(this.url);
   }
 
-  async closeBrowser() {
+  /**
+   * Closes the browser.
+   *
+   * @async
+   * @throws {Error} If browser is not initialized
+   * @returns {Promise<void>} A promise that resolves when the browser is closed
+   */
+  closeBrowser = async() => {
     if (!this.browser) {
       throw new Error('Browser is not initialized. Nothing to close.');
     }
